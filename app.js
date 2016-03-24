ad860f9154e0e98650e83cc3dec5c67ba5c30dff
Yves Medhard Tibe da Cunha Tibe-Bi <yvesmedhard@outlook.com>
2016-03-23 22:27:30 -0300

    Initial Commit

---
+++ app.js
@@ -0,0 +1,13 @@
+//Requires Modules
+var express = require('express')(); //Requires Express module for Routing and REST management
+
+
+
+
+/*
+
+express.get('/', function(req, res){
+  res.send('hello world');
+});
+
+express.listen(3000);*/
